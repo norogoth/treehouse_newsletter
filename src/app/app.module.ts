ddb39ce93fc54207cc7627c38e7f981d138a264c
norogoth <norogoth@gmail.com>
2021-06-14 15:43:47 -0700

    Starting project. npm adit says bad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserListComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
